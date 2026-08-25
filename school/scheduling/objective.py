3604dc57c31e8ddae09089409b9170c3afa6cdf5
husseintech <husseintech@users.noreply.github.com>
2026-08-25 22:26:53 +0300

    حذف قسم البرامج الأسبوعية (الخطط/المحرك) والرجوع للجدول اليومي اليدوي للمعلمين
    - إزالة نماذج SchedulePlan/ScheduleEntry/TeachingLoad/التوفر/القيود/الحصص المثبتة وهجراتها (مهاجرة 0039)
    
    - حذف حزمة school/scheduling و scheduling_engine.py و tests_schedule.py
    
    - حذف شاشات/روابط النظام الجديد من views/urls/templates
    
    - الإبقاء على TeacherScheduleEntry ونظام الجدول اليومي اليدوي وتقاريره (schedule_edit/schedule_print_*)
    
    - manage.py check يمر بلا أخطاء

--- school/scheduling/objective.py
+++
@@ -1,9 +0,0 @@
-from ortools.sat.python import cp_model as cp
-
-
-def assemble(model, penalties):
-    """يضيف دالة الهدف: تصغير مجموع العقوبات (القيود التفضيلية)."""
-    if penalties:
-        model.Minimize(sum(expr * w for expr, w in penalties))
-    else:
-        model.Minimize(0)
