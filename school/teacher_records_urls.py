c28b7024bd2b241217dd439ff91134c59753fd4a
husseintech <h2a12009@gmail.com>
2026-09-02 14:54:56 +0300

    Link teaching load distribution report

--- school/teacher_records_urls.py
+++ school/teacher_records_urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import teacher_records_views as v
 from .teacher_records_reset import teacher_records_reset
 from .class_subject_views import class_subject_mapping
+from .teaching_load_views import teaching_load_distribution
 
 urlpatterns = [
     path('class-subjects/', class_subject_mapping, name='class_subject_mapping'),
+    path('load-distribution/', teaching_load_distribution, name='teaching_load_distribution'),
     path('curriculum/', v.curriculum_records, name='curriculum_records'),
     path('curriculum/print/', v.curriculum_records_print, name='curriculum_records_print'),
     path('curriculum/<int:record_id>/delete/', v.curriculum_record_delete, name='curriculum_record_delete'),
