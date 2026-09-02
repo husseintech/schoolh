2e37e7a8420370785f855f8b711637e5c83cd297
husseintech <h2a12009@gmail.com>
2026-09-02 11:26:58 +0300

    Wire note delete route

--- school_management/urls.py
+++ school_management/urls.py
@@ -4,11 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from school.academic_report_views import academic_achievement_report
+from school.note_actions import delete_note
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('administration/academic-achievement/', academic_achievement_report, name='academic_achievement_report'),
     path('administration/teacher-records/', include('school.teacher_records_urls')),
+    path('notes/<int:note_id>/delete/', delete_note, name='delete_note'),
     path('', include('school.supervisor_followup_urls')),
     path('', include('school.urls')),
     path('open-learning/', include('open_learning.urls')),
